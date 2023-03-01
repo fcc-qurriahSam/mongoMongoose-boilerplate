81b39641a87ef0c5fc474736a8f82263964d6130
qurriahSam <mugirethe1@gmail.com>
2023-03-01 20:47:11 +0300

    ch: create mongoose connection

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
-
+require("dotenv").config();
+const mongoose = require("mongoose");
+const uri = process.env.MONGO_URI;
+mongoose.connect(`${uri}`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
