e8e3c408360fedc78cc48d54f88c1399b233490a
qurriahSam <mugirethe1@gmail.com>
2023-03-16 03:19:51 +0300

    fx: change person case

--- myApp.js
+++ myApp.js
@@ -97,7 +97,7 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-  person.findOneAndUpdate({ name: personName }, { age: ageToSet }, { new: true }, (err, data) => {
+  Person.findOneAndUpdate({ name: personName }, { age: ageToSet }, { new: true }, (err, data) => {
     if (err) return console.log(err);
     done(null, data);
   });
